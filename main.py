e40764f290b6163f4f7803d5d2fd8f90a1694982
martincorona007 <martincorona007@gmail.com>
2019-02-08 21:17:54 -0600

    file

---
+++ main.py
@@ -0,0 +1,113 @@
+print("What's your Hackerman name?")
+name=input("Write your name: ")
+lastName=input("Write your last name: ")
+lisst=[]
+
+if(('A'==name[0]) or ('a'==name[0])):
+    lisst.append('SH4DOW')
+elif(('B'== name[0]) or ('b'==name[0])):
+    lisst.append('CHIEF')
+elif(('C'==name[0])or('c'==name[0])):
+    lisst.append('CYBER')
+elif(('D'==name[0])or('d'==name[0])):
+    lisst.append('M4STER')
+elif(('E'==name[0])or('e'==name[0])):
+    lisst.append('PRIV4TE')
+elif(('F'==name[0])or('f'==name[0])):
+    lisst.append('NULL')
+elif(('G'==name[0])or('g'==name[0])):
+    lisst.append('TESTED')
+elif(('H'==name[0])or('h'==name[0])):
+    lisst.append('GENER4L')
+elif(('I'==name[0])or('i'==name[0])):
+    lisst.append('TOXIC')
+elif(('J'==name[0])or('j'==name[0])):
+    lisst.append('CRASH')
+elif(('K'==name[0])or('k'==name[0])):
+    lisst.append('STEALPH')
+elif(('L'==name[0])or('l'==name[0])):
+    lisst.append('H4ACKER')
+elif(('M'==name[0])or('m'==name[0])):
+    lisst.append('UBER')
+elif(('N'==name[0])or('n'==name[0])):
+    lisst.append('GRAY')
+elif(('O'==name[0])or('o'==name[0])):
+    lisst.append('MR.')
+elif(('P'==name[0])or('p'==name[0])):
+    lisst.append('TROLL')
+elif(('Q'==name[0])or('q'==name[0])):
+    lisst.append('ASYNC')
+elif(('R'==name[0])or('r'==name[0])):
+    lisst.append('RENA')
+elif(('S'==name[0])or('s'==name[0])):
+    lisst.append('NINJA')
+elif(('T'==name[0])or('t'==name[0])):
+    lisst.append('TRUE')
+elif(('U'==name[0])or('u'==name[0])):
+    lisst.append('JAVA')
+elif(('V'==name[0])or('v'==name[0])):
+    lisst.append('MYSQL')
+elif(('W'==name[0])or('w'==name[0])):
+    lisst.append('TESTED')
+elif(('Y'==name[0])or('y'==name[0])):
+    lisst.append('FIRE')
+elif(('Z'==name[0])or('z'==name[0])):
+    lisst.append('ZERO')
+
+if (('A' == lastName[0]) or ('a' == lastName[0])):
+    lisst.append('BET4')
+elif (('B' == lastName[0]) or ('b' == lastName[0])):
+    lisst.append('TERABYTE')
+elif (('C' == lastName[0]) or ('c' == lastName[0])):
+    lisst.append('WIRE')
+elif (('D' == lastName[0]) or ('d' == lastName[0])):
+    lisst.append('PL4GUE')
+elif (('E' == lastName[0]) or ('e' == lastName[0])):
+    lisst.append('SKELETON')
+elif (('F' == lastName[0]) or ('f' == lastName[0])):
+    lisst.append('B4RCODE')
+elif (('G' == lastName[0]) or ('g' == lastName[0])):
+    lisst.append('FRE4K')
+elif (('H' == lastName[0]) or ('h' == lastName[0])):
+    lisst.append('BOT')
+elif (('I' == lastName[0]) or ('i' == lastName[0])):
+    lisst.append('SSL')
+elif (('J' == lastName[0]) or ('j' == lastName[0])):
+    lisst.append('HYDRA4')
+elif (('K' == lastName[0]) or ('k' == lastName[0])):
+    lisst.append('BYTE')
+elif (('L' == lastName[0]) or ('l' == lastName[0])):
+    lisst.append('PH4NTHOM')
+elif (('M' == lastName[0]) or ('m' == lastName[0])):
+    lisst.append('MON')
+elif (('N' == lastName[0]) or ('n' == lastName[0])):
+    lisst.append('NIGHT')
+elif (('O' == lastName[0]) or ('o' == lastName[0])):
+    lisst.append('CRACK')
+elif (('P' == lastName[0]) or ('p' == lastName[0])):
+    lisst.append('SHARK')
+elif (('Q' == lastName[0]) or ('q' == lastName[0])):
+    lisst.append('VARY4BLE')
+elif (('R' == lastName[0]) or ('r' == lastName[0])):
+    lisst.append('ROOT')
+elif (('S' == lastName[0]) or ('s' == lastName[0])):
+    lisst.append('ANTRAX')
+elif (('T' == lastName[0]) or ('t' == lastName[0])):
+    lisst.append('VIRUS')
+elif (('U' == lastName[0]) or ('u' == lastName[0])):
+    lisst.append('SILENCE')
+elif (('V' == lastName[0]) or ('v' == lastName[0])):
+    lisst.append('SAINT')
+elif (('W' == lastName[0]) or ('w' == lastName[0])):
+    lisst.append('DESTROYER')
+elif (('Y' == lastName[0]) or ('y' == lastName[0])):
+    lisst.append('L3G3ND')
+elif (('Z' == lastName[0]) or ('z' == lastName[0])):
+    lisst.append('PROGRAM')
+
+print('Your Hackerman name is: '+lisst[0]+' '+lisst[1])
+
+
+
+
+
